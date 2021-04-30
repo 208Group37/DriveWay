972affb8339c46bb9143ebe367b05036b72f2934
christian-carroll <78798341+christian-carroll@users.noreply.github.com>
2021-04-30 11:09:09 +0100

    Availability times added to profile

--- DriveWay/Instructor View Controllers/InstructorProfileViewController.swift
+++ DriveWay/Instructor View Controllers/InstructorProfileViewController.swift
@@ -18,11 +18,19 @@ class InstructorProfileViewController: UIViewController {
     @IBOutlet weak var CarMakeModelLabel: UILabel!
     @IBOutlet weak var fuelAndTransmissionLabel: UILabel!
     @IBOutlet weak var registrationLabel: UILabel!
-    @IBOutlet weak var priceLabel: UITextField!
+    @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var lessonLengthLabel: UILabel!
     @IBOutlet weak var crashCourseLabel: UILabel!
     @IBOutlet weak var distanceLabel: UILabel!
     @IBOutlet weak var lessonCountLabel: UILabel!
+    @IBOutlet weak var mondayTimesLabel: UILabel!
+    @IBOutlet weak var tuesdayTimesLabel: UILabel!
+    @IBOutlet weak var wednesdayTimesLAbel: UILabel!
+    @IBOutlet weak var thursdayTimesLabel: UILabel!
+    @IBOutlet weak var fridayTimesLabel: UILabel!
+    @IBOutlet weak var saturdayTimesLabel: UILabel!
+    @IBOutlet weak var sundayTimesLabel: UILabel!
+    @IBOutlet weak var aboutMeBox: UITextField!
     
     
     let userInfo = Auth.auth().currentUser
@@ -36,6 +44,14 @@ class InstructorProfileViewController: UIViewController {
         
 
         // Do any additional setup after loading the view.
+    }
+    
+    func convertArrayToString(array : [String]) -> String {
+        var outputString = ""
+        for item in array {
+            outputString = outputString + ", " + item
+        }
+        return outputString
     }
     
     func documentRetrieve() {
@@ -59,11 +75,31 @@ class InstructorProfileViewController: UIViewController {
                     self.fuelAndTransmissionLabel.text = carInfo!["fuelType"]! + ", " + carInfo!["transmission"]!
                     self.registrationLabel.text = carInfo!["numberPlate"]
                     
+                    let lessonInfo = userDocData["lesson"] as? [String: Any]
+                    self.priceLabel.text = lessonInfo!["price"] as? String
+                    var crashCourseString: String
+                    if lessonInfo!["crashCourses"] as! Bool == true {
+                        crashCourseString = "Yes"
+                    } else {
+                        crashCourseString = "No"
+                    }
+                    self.crashCourseLabel.text = crashCourseString
+                    
+                    let times = userDocData["availability"] as? [String : Array<String>]
+                    self.mondayTimesLabel.text = self.convertArrayToString(array: times!["monday"] ?? [])
+                    self.tuesdayTimesLabel.text = self.convertArrayToString(array: times!["tuesday"] ?? [])
+                    self.wednesdayTimesLAbel.text = self.convertArrayToString(array: times!["wednesday"] ?? [])
+                    self.thursdayTimesLabel.text = self.convertArrayToString(array: times!["thursday"] ?? [])
+                    self.fridayTimesLabel.text = self.convertArrayToString(array: times!["friday"] ?? [])
+                    self.saturdayTimesLabel.text = self.convertArrayToString(array: times!["saturday"] ?? [])
+                    self.sundayTimesLabel.text = self.convertArrayToString(array: times!["sunday"] ?? [])
+                    
                     
                 }
             }
         }
     }
+    
 
     /*
     // MARK: - Navigation
